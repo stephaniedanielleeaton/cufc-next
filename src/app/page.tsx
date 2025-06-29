083b694f275fee9e24c24c5dfbaf88595eb52952
Stephanie <stephaniedanielleeaton@gmail.com>
2025-06-29 18:07:38 -0400

    resolving es lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { useUser } from "@auth0/nextjs-auth0";
 
-import Image from "next/image";
-
+0
 export default function Home() {
 
-  const { user, isLoading } = useUser();
 
   return (
     <>
