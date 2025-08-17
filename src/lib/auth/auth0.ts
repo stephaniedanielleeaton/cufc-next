e93e8aab80786ac3e8a57b8dbec1a6adf50f659b
Stephanie <stephaniedanielleeaton@gmail.com>
2025-08-17 16:44:05 -0400

    fix auth to use default login and callback

--- src/lib/auth/auth0.ts
+++ src/lib/auth/auth0.ts
@@ -4,10 +4,5 @@ export const auth0 = new Auth0Client({
   authorizationParameters: {
     scope: "openid profile email",
     audience: "columbusunitedfencing.com",
-  },
-  routes: {
-    callback: "/api/auth/callback",
-    login: "/api/auth/login",
-    postLoginRedirect: "/dashboard"
-  },
+  }
 });
